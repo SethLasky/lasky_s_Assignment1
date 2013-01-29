3965143b11edc7f891744483446bb734c38e306b
Seth Lasky <sethlasky@hotmail.com>
2013-01-29 14:24:37 -0500

    Addition to program
    Added binSearch method, added seqSearch method, created for loop to find
    the number of iterations in each sequential search

---
+++ lasky_s_Unit1Assignment.java
@@ -0,0 +1,63 @@
+public class lasky_s_Unit1Assignment
+{
+   public static void main(String[] args)
+   {
+     //1. Create Sorted List of 20 numbers
+     int[] list = {4, 9, 17, 35, 46, 55, 63, 80, 81, 89, 92, 95, 102, 113, 128, 143, 162, 169, 171, 199};
+     int linearIteration = 0;
+     int binaryIteration = 0;
+     for(int x = 0; x < list.length; x++)
+     {
+       int key = list[x];
+       int linear = seqSearch(list, key);
+       System.out.println("Using Linear Search the number of iterations to find " + key
+                            + " is " + linear);
+       linearIteration = linearIteration + linear;
+       
+     
+       
+     }
+
+     
+     
+   }
+   
+   public static int binSearch(int [] array, int searchItem)
+    {
+       int iterations = 0;
+       int start = 0;
+       int end = array.length-1;
+       int mid;
+       while (start <= end)
+       {
+    iterations++;
+           mid = (start+end)/2;
+           if (searchItem == array[mid])
+             {
+               return iterations;
+             }
+           else if (searchItem < array[mid])
+             {
+               end = mid-1;
+             }
+           else
+             {
+               start = mid+1;
+             }
+       }
+       return -1;
+    }
+   
+   public static int seqSearch( int[] array, int searchItem )
+  {
+ int iterations = 0;
+     for ( int j=0; j  < array.length; j++ ){
+  iterations++;
+          if ( array[j] == searchItem)
+   return iterations; 
+     }
+     return -1 ; 
+  }
+   
+   
+}
